2de545a44839c87db12b473ec4f6fa89fb4c54aa
friendbear <friendbear@users.noreply.github.com>
2021-09-02 15:50:59 +0900

    crate changed.

--- src/bin/books.rs
+++ src/bin/books.rs
@@ -1,17 +1,24 @@
-use mongodb::options::ClientOptions;
-use mongodb::{
-    bson::{doc, Document},
-    Client,
-};
+use mongodb::{bson::{DateTime, bson, doc}, options::{
+        ResolverConfig,
+        ClientOptions,
+        FindOptions
+    }};
 use std::env;
-use serde::{Deserialize, Serialize};
 use std::error::Error;
-use chrono::prelude::*;
-// use tokio;
-use trust_dns_resolver::config::*;
+use serde::{Deserialize, Serialize};
+use tokio;
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Book {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    id: Option<mongodb::bson::oid::ObjectId>,
+    title: String,
+    authors: Vec<String>,
+    publisher: String,
+    launch: DateTime,
+}
 
-#[async_std::main]
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>>{
 
     let client_url =
@@ -21,15 +28,33 @@ async fn main() -> Result<(), Box<dyn Error>>{
         ClientOptions::parse_with_resolver_config(&client_url, ResolverConfig::cloudflare()).await?;
     let client = mongodb::Client::with_options(options)?;
 
-    let books = client.database("booklog").collection("books");
+    //let books = client.database("booklog").collection_with_type::<Book>("books");
+    let mut books = client.database("booklog").collection("books");
 
     let count = books.count_documents(
-        doc!{},
+        doc!{
+            "publisher": "講話社"
+        },
         None
     ).await?;
 
     println!("books stored: {:?}", count);
 
+    let options = FindOptions::builder().sort({
+            doc!{
+                "launch": -1
+            }
+        }
+    ).build();
+    let mut result  = books.find(
+        None,
+        None,
+    ).await?;
+
+    println!("{:?}", &result);
+    while let Some(doc) = result.next().await {
+        println!("{:?}", doc?);
+    }
     // let book_json: serde_json::Value = Bson::from(None).into();
 
     // println!("JSON: {:?}", user_json);
